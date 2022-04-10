364ca14129445b863020401697c8051b75cc49a0
Jamie Gaehring <gaehj457@gmail.com>
2022-04-10 17:42:02 -0400

    Move pod layout to card components.

---
+++ docs/components/pod-card.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-card>
+    <h3>{{ pod.name }}</h3>
+    <p v-if="pod.description">{{ pod.description }}</p>
+    <ul>
+      <li v-if="pod.channel">
+        Discord:&nbsp;
+        <a :href="pod.channel" target="_blank">
+          <code>{{ `#pod_${pod.name.toLowerCase()}` }}</code>
+        </a>
+      </li>
+    </ul>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from './base-card.vue';
+
+export default {
+  name: 'PodCard',
+  components: {
+    BaseCard,
+  },
+  props: ['pod'],
+}
+</script>
+
+<style scoped>
+
+</style>>
--- docs/index.md
+++ docs/index.md
@@ -8,20 +8,12 @@ Here is an example of the info we might include for each pod,
 as defined in `./pods.yml`:
 
 <script setup>
-  import pods from './pods.yml'
+  import pods from './pods.yml';
+  import PodCard from './components/pod-card.vue';
 </script>
 
 <div v-for="pod in pods">
-  <h3>{{pod.name}}</h3>
-  <p>{{pod.description}}</p>
-  <ul>
-    <li>
-      Discord:&nbsp;
-      <a :href="pod.channel" target="_blank">
-        <code>{{`#pod_${pod.name.toLowerCase()}`}}</code>
-      </a>
-    </li>
-  </ul>
+  <pod-card :pod="pod"/>
 </div>
 
 ## Regions
