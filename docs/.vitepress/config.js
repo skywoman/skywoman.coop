7faa59f8fce688ba526da6d04c482dde6f124960
Jamie Gaehring <gaehj457@gmail.com>
2022-04-10 09:42:34 -0400

    Support loading YAML etc via vite-plugin-content.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,11 @@
+const content = require('@originjs/vite-plugin-content').default;
+
 module.exports = {
   title: 'skywoman.coop',
   description: 'a platform for communities to develop their food sovereignty.',
+  vite: {
+    plugins: [
+      content(),
+    ],
+  },
 };
